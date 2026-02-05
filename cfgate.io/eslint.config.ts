ae3416f719fc101ae3dcf56c69ffc7f3f074faba
mannie-exe <mannie@inherent.design>
2026-02-05 10:38:23 -0800

    chore: fix cfgate.io bootstrap

--- cfgate.io/eslint.config.ts
+++ cfgate.io/eslint.config.ts
@@ -1,19 +1,32 @@
 import js from '@eslint/js'
 import { defineConfig } from 'eslint/config'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default defineConfig([
-  tseslint.configs.recommended,
-  {
-    files: ['src/**'],
-    languageOptions: {
-      globals: {
-        // TODO: configure Cloudflare Workers globals? Include ./worker-configuration.d.ts?
-      },
-    },
-    plugins: {
-      js,
-    },
-    extends: ['js/recommended'],
+export default defineConfig({
+  rules: {
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
-])
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+      ...globals.serviceworker,
+    },
+    parser: tseslint.parser,
+    parserOptions: {
+      ecmaFeatures: {
+        impliedStrict: true,
+      },
+      projectService: true,
+      sourceType: 'module',
+    },
+  },
+  plugins: {
+    js: js,
+    tseslint: tseslint,
+  },
+  extends: [js.configs.recommended, tseslint.configs.stylisticTypeChecked],
+  files: ['src/**'],
+  ignores: ['docs/**', 'node_modules/**'],
+})
--- cfgate.io/vitest.config.ts
+++ cfgate.io/vitest.config.ts
@@ -1,14 +1,17 @@
-import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config"
+import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
 
 export default defineWorkersConfig({
+  esbuild: {
+    exclude: ['node_modules', 'docs'],
+  },
   test: {
     poolOptions: {
       workers: {
-        wrangler: { configPath: "./wrangler.toml" },
+        wrangler: { configPath: './wrangler.toml' },
         // miniflare: {
         //   kvNamespaces: ["TEST_NAMESPACE"],
         // },
       },
     },
   },
-});
+})
